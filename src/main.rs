1e8a2c2f9659320c3d38f6ccb29f5f66ce4b9170
fengtuo <2693263558@qq.com>
2022-11-29 08:33:03 +0000

    rand

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("you guessed:{}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+    // println!("Please input your guess.");
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // println!("you guessed:{}", guess);
 }
